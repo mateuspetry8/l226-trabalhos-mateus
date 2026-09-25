58bdec0631a16e156678488a0c367c48ae73072f
Mateus <mateusp.petry@gmail.com>
2026-09-25 17:34:38 -0300

    implementei l_destroi, l_tam, l_cheia, l_vazia, l_imprime, l_insere inicio e fim

--- entrega-t3/lista.c
+++ entrega-t3/lista.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 typedef struct No {
-    int dado;
+    dado_t dado;
     struct No *prox;
     struct No *ant;
 } No;
@@ -16,13 +16,19 @@ struct lista{
 
 Lista l_cria()
 {
-    Lista l = malloc(sizeof(Lista));
+    Lista l = malloc(sizeof(*l));
     l->sentinela = malloc(sizeof(No));
     l->sentinela->prox = l->sentinela;
     l->sentinela->ant = l->sentinela;
     return l;
 }
 
+// cria uma lista contendo substrings de s
+// as substrings são separadas por quaisquer caractere de sep
+// os caracteres de sep não aparecem nas substrings
+// exemplos:
+//   "a,ba,ca, te", ", " -> ["a" "ba" "ca" "te"]
+//   "aba \ncate\n", "\n" -> ["aba " "cate"]
 Lista l_cria_separando(Str s, Str sep)
 {
 
@@ -31,43 +37,75 @@ Lista l_cria_separando(Str s, Str sep)
 // libera a memória ocupada por uma lista
 void l_destroi(Lista l)
 {
-
+    No *atual = l->sentinela->prox;
+    while(atual != l->sentinela) {
+        No *prox = atual->prox;
+        free(atual);
+        atual = prox;
+    }
+    free(l->sentinela);
+    free(l);
+    //talvez free em Str
 }
 
 // retorna o número de elementos na lista
 int l_tam(Lista l)
 {
+    int tam = 0;
+    No *atual = l->sentinela;
 
+    while(atual->prox != l->sentinela) {
+        tam++;
+        atual = atual->prox;
+    }
+
+    return tam;
 }
 
 // retorna true se a lista tiver cheia
 bool l_cheia(Lista l)
 {
-
+    return false;
 }
 
 // retorna true se a lista tiver vazia
 bool l_vazia(Lista l)
 {
-
+    return l->sentinela->prox == l->sentinela;
 }
 
 // imprime os dados que estão na lista
 void l_imprime(Lista l)
 {
+    No *atual = l->sentinela->prox;
 
+    while (atual != l->sentinela)
+    {
+        s_imprime(atual->dado);
+        atual = atual->prox;
+    }
 }
 
 // insere o dado d no início da lista l
 void l_insere_inicio(Lista l, dado_t d)
 {
-
+    No *novo = malloc(sizeof(No));
+    novo->prox = l->sentinela->prox;
+    novo->ant = l->sentinela;
+    novo->dado = d;
+    novo->prox->ant = novo;
+    l->sentinela->prox = novo;
 }
 
 // insere o dado d no final da lista l
 void l_insere_fim(Lista l, dado_t d)
 {
-
+    No *novo = malloc(sizeof(No));
+    novo->prox = l->sentinela;
+    novo->ant = l->sentinela->ant;
+    novo->dado = d;
+    novo->ant->prox = novo;
+    l->sentinela->ant = novo;
 }
 
 // insere o dado d na lista l, de forma que ele fique na posição p
